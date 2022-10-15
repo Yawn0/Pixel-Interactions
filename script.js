a254ee7481add510dc0956544ee72c1ec6ddf063
Yawn0 <77552501+Yawn0@users.noreply.github.com>
2022-10-15 16:16:02 +0200

    Added movement and velocity to parcticles

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ window.addEventListener('load', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     
-    const image1 = document.getElementById('image1');
 
     class Particle{
         constructor(effect) {
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
-            this.size = 30;
+            this.size = 10;
+            this.vx = Math.random() * 2 - 1;
+            this.vy = Math.random() * 2 - 1;
         }
 
         draw(context){
             context.fillRect(this.x, this.y, this.size, this.size);
+        }
+        update(){
+            this.x += this.vx;
+            this.y += this.vy;
         }
     }
 
@@ -25,22 +30,38 @@ window.addEventListener('load', function() {
             this.width = width;
             this.height = height;
             this.particlesArray = [];
+            this.image = document.getElementById('image1');
+            this.centerX = this.width * 0.5;
+            this.centerY = this.height * 0.5;
+            this.x = this.centerX - this.image.width * 0.5;
+            this.y = this.centerY - this.image.height * 0.5;
         }
         init(){
-            this.particlesArray.push(new Particle(this));
+            for(let i = 0; i < 100; i++){
+                this.particlesArray.push(new Particle(this));
+            }
         }
         draw(context){
             this.particlesArray.forEach(particle => particle.draw(context));
+            context.drawImage(this.image, this.x, this.y);
+        }
+        update(){
+            this.particlesArray.forEach(particle => particle.update());
         }
     }
 
     const effect = new Effect(canvas.width, canvas.height);
     effect.init();
-    effect.draw(ctx);
-
+    
     function animate(){
-
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        // ctx.beginPath();
+        effect.draw(ctx);
+        effect.update();
+        requestAnimationFrame(animate);
     }
+
+    animate();
 
     // ctx.fillRect(120, 120 , 100, 200);
     // ctx.drawImage(image1, 0, 0, 500, 800);
